aea72a20510febe78f0f9d1925c1ae64fe7ea80b
ensbakirog <ensbakirog@gmail.com>
2025-10-26 18:50:12 +0300

    Basic UI done Dynamic Engine Temp Bar done

--- CarMonitor/ViewModels/MainWindowViewModel.cs
+++ CarMonitor/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,42 @@
-﻿namespace CarMonitor.ViewModels;
+﻿using CarMonitor.Models;
+using CarMonitor.Services;
+
+namespace CarMonitor.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    private readonly Obd2DataService _obd2Service;
+
+    public MainWindowViewModel()
+    {
+        _obd2Service = new Obd2DataService();
+        _obd2Service.PropertyChanged += OnObd2DataChanged;
+    }
+
+    public VehicleData VehicleData => _obd2Service.VehicleData;
+    public ChartData ChartData => _obd2Service.ChartData;
+    public bool IsConnected => _obd2Service.IsConnected;
+    public string ConnectedCarName => "Porsche";
+
+    private void OnObd2DataChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(Obd2DataService.VehicleData))
+        {
+            OnPropertyChanged(nameof(VehicleData));
+        }
+        else if (e.PropertyName == nameof(Obd2DataService.ChartData))
+        {
+            OnPropertyChanged(nameof(ChartData));
+        }
+        else if (e.PropertyName == nameof(Obd2DataService.IsConnected))
+        {
+            OnPropertyChanged(nameof(IsConnected));
+        }
+    }
+
+    public override void Dispose()
+    {
+        _obd2Service?.Dispose();
+        base.Dispose();
+    }
 }
--- CarMonitor/ViewModels/ViewModelBase.cs
+++ CarMonitor/ViewModels/ViewModelBase.cs
@@ -1,7 +1,12 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace CarMonitor.ViewModels;
 
-public class ViewModelBase : ObservableObject
+public class ViewModelBase : ObservableObject, IDisposable
 {
+    public virtual void Dispose()
+    {
+        // Override in derived classes if needed
+    }
 }
