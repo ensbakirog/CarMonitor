32d5d785056aef2f8a880be36833ee77306c72ee
ensbakirog <ensbakirog@gmail.com>
2025-11-04 11:26:28 +0300

    Auto Wifi Finding and Connection added

--- CarMonitor/ESP8266/ESP8266.ino
+++ CarMonitor/ESP8266/ESP8266.ino
@@ -1,18 +1,26 @@
 #include <ESP8266WiFi.h>
 #include <WebSocketsServer.h>
 
-#define ANSI_RESET   "\x1b[0m"  // Tüm renkleri ve stilleri sıfırlar
-#define ANSI_RED     "\x1b[31m"  // Kırmızı renk
-#define ANSI_GREEN   "\x1b[32m"  // Yeşil renk
-#define ANSI_YELLOW  "\x1b[33m"  // Sarı renk
-#define ANSI_BLUE    "\x1b[34m"  // Mavi renk
-#define ANSI_MAGENTA "\x1b[35m"  // Macenta renk
-#define ANSI_CYAN    "\x1b[36m"  // Camgöbeği (Cyan) renk
+#define ANSI_RESET   "\x1b[0m"
+#define ANSI_RED     "\x1b[31m"
+#define ANSI_GREEN   "\x1b[32m"
+#define ANSI_YELLOW  "\x1b[33m"
+#define ANSI_BLUE    "\x1b[34m"
+#define ANSI_MAGENTA "\x1b[35m"
+#define ANSI_CYAN    "\x1b[36m"
 
-const char* HOTSPOT_SSID = "Alien";
-const char* HOTSPOT_PASSWORD = "pK3169@31";
-const char* WIFI_SSID = "Andromeda24";
-const char* WIFI_PASSWORD = "AG?6<mE!8WAC0XgXq";
+struct WifiNetwork {
+  const char* ssid;
+  const char* password;
+};
+
+const WifiNetwork knownNetworks[] = {
+  {"Alien", "pK3169@31"}
+  // {"Andromeda24", "AG?6<mE!8WAC0XgXq"}
+};
+
+const int numKnownNetworks = sizeof(knownNetworks) / sizeof(knownNetworks[0]);
+
 const uint16_t WEBSOCKET_PORT = 1337;
 const long SERIAL_BAUD_RATE = 115200;
 const char PACKET_TERMINATOR = '\n';
@@ -20,7 +28,6 @@ const char PACKET_TERMINATOR = '\n';
 WebSocketsServer webSocket(WEBSOCKET_PORT);
 String stm32DataBuffer = "";
 bool stm32DataReady = false;
-
 unsigned long lastWifiCheckTime = 0;
 const long wifiCheckInterval = 5000;
 
@@ -48,31 +55,94 @@ void loop() {
 
 void setupSerial() {
   Serial.begin(SERIAL_BAUD_RATE);
-  delay(3000);
+  unsigned long startTime = millis();
+
+  while (!Serial || (millis() - startTime < 3000)) {}
+
   Serial.println();
   Serial.println(ANSI_GREEN "Serial initialized" ANSI_RESET);
 }
 
 void setupWiFi() {
-  Serial.print("Connecting to ");
-  Serial.print(ANSI_MAGENTA);
-  Serial.print(HOTSPOT_SSID);
-  Serial.println(ANSI_RESET);
-  Serial.println();
+  bool connected = false;
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
 
-  WiFi.begin(HOTSPOT_SSID, HOTSPOT_PASSWORD);
+  while (!connected) {
+    Serial.println(ANSI_YELLOW "Scanning for WiFi networks..." ANSI_RESET);
+    
+    int numNetworks = WiFi.scanNetworks();
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println(ANSI_YELLOW "<< ESP8266 connecting to WIFI >>" ANSI_RESET);
+    if (numNetworks == 0) {
+      Serial.println(ANSI_RED "No networks found. Retrying in 5 seconds..." ANSI_RESET);
+      delay(5000);
+      continue;
+    }
+
+    Serial.println();
+    Serial.printf("%d networks found:\n", numNetworks);
+
+    for (int i = 0; i < numNetworks; ++i)
+      Serial.printf("  %d: %s\n", i + 1, WiFi.SSID(i).c_str());
+
+    Serial.println();
+
+    bool knownNetworkFound = false;
+
+    for (int i = 0; i < numNetworks; i++) {
+      String scannedSSID = WiFi.SSID(i);
+
+      for (const auto& net : knownNetworks) { 
+        
+        if (scannedSSID == net.ssid) { 
+          knownNetworkFound = true;
+          Serial.print("Known network found: ");
+          Serial.print(ANSI_CYAN);
+          Serial.print(net.ssid);
+          Serial.println(ANSI_RESET);
+          Serial.println(ANSI_YELLOW "Attempting to connect..." ANSI_RESET);
+
+          WiFi.begin(net.ssid, net.password);
+
+          int timeoutCounter = 30;
+          while (WiFi.status() != WL_CONNECTED && timeoutCounter > 0) {
+            delay(500);
+            Serial.println(ANSI_YELLOW "Trying to connect..." ANSI_RESET);
+            timeoutCounter--;
+          }
+          Serial.println(); 
+
+          if (WiFi.status() == WL_CONNECTED) {
+            Serial.println(ANSI_GREEN "WiFi connected!" ANSI_RESET);
+            Serial.print("IP address: ");
+            Serial.print(ANSI_CYAN);
+            Serial.print(WiFi.localIP());
+            Serial.println(ANSI_RESET);
+            connected = true; 
+            return;
+          } else {
+            Serial.println(ANSI_RED "Connection failed." ANSI_RESET);
+            WiFi.disconnect(); 
+            break;
+          }
+        }
+      }
+      
+      if(connected) break;
+      
+    }
+
+    if (!connected) {
+      if (knownNetworkFound) {
+        Serial.println(ANSI_YELLOW "Found known networks, but failed to connect to any." ANSI_RESET);
+      } else {
+        Serial.println(ANSI_YELLOW "No known networks found in this scan." ANSI_RESET);
+      }
+      Serial.println("Retrying scan in 5 seconds...");
+      delay(5000);
+    }
   }
-
-  Serial.println();
-  Serial.println(ANSI_GREEN "WiFi connected" ANSI_RESET);
-  Serial.print("IP address: ");
-  Serial.print(ANSI_CYAN);
-  Serial.print(WiFi.localIP());
-  Serial.println(ANSI_RESET);
 }
 
 void setupWebSocket() {
@@ -99,10 +169,8 @@ void handleSerialReception() {
   if (stm32DataReady) {
     return;
   }
-
   while (Serial.available() > 0) {
     char inChar = (char)Serial.read();
-
     if (inChar == PACKET_TERMINATOR) {
       stm32DataReady = true;
       break;
